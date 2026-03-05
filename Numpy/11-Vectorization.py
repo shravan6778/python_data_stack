2ef2fa06a151132128e1b14c950d6ac45d79a2fc
shravan6778 <jaligamshravan2004@gmail.com>
2026-03-05 21:30:54 +0530

    Vectorization

---
+++ Numpy/11-Vectorization.py
@@ -0,0 +1,6 @@
+import numpy as np
+#vectorization is the process of applying operations to entire arrays at once, rather than using explicit Python loops to process individual elements sequentially
+arr1=np.array([1,2,3])
+arr2=np.array([4,5,6])
+print(arr1+arr2)
+print(arr1*10)
