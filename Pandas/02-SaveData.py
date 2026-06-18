3704c8bcd3d25dbe73cd6a23acaff0109bfbb921
shravan6778 <jaligamshravan2004@gmail.com>
2026-06-18 11:58:21 +0530

    What is DataFrame?

--- Pandas/02-SaveData.py
+++ Pandas/02-SaveData.py
@@ -1,3 +1,6 @@
+'''A DataFrame is a two-dimensional, spreadsheet-like data structure in Pandas that organizes data into rows and columns. It is the primary tool used in Python for storing, filtering, and manipulating tabular data efficiently.
+'''
+
 import pandas as pd
 data={
     "Name":['Shravan','Sandy','Ceo Of India'],
